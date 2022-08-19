90fcabf220bf6cda5c00a5c8103a4c86fc4eb210
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-08-19 20:50:18 +0600

    set alternative state

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import './App.css';
 import '../src/components/style.css'
 import Game from '../src/components/Game'
 import { useState } from 'react';
-const initialState = ['', '', '', '', '', '', '', '', '']
+const initialState = ['', '', '', '', '', '', '', '', ''];
 
 function App() {
   const [gameState, setGAmeState] = useState(initialState);
+  const [changeState, setChangeState] = useState(false)
+
+  const clicked = (index) => {
+    let strings = Array.from(gameState);
+    if (strings[index])
+      return;
+    strings[index] = changeState ? "X" : "0";
+    setChangeState(!changeState)
+    setGAmeState(strings)
+
+  }
   return (
     <div className="app-header">
       <button>RESET GAME</button>
       <p className='heading-text'>TIC TAC TOE</p>
       <div className='row jc-center'>
-        <Game className="b-bottom-right" state={gameState[0]} />
-        <Game className="b-bottom-right" state={gameState[1]} />
-        <Game className="b-bottom" state={gameState[2]} />
+        <Game className="b-bottom-right" state={gameState[0]} onClick={() => clicked(0)} />
+        <Game className="b-bottom-right" state={gameState[1]} onClick={() => clicked(1)} />
+        <Game className="b-bottom" state={gameState[2]} onClick={() => clicked(2)} />
       </div>
       <div className='row jc-center'>
-        <Game className="b-bottom-right" state={gameState[3]} />
-        <Game className="b-bottom-right" state={gameState[4]} />
-        <Game className="b-bottom" state={gameState[5]} />
+        <Game className="b-bottom-right" state={gameState[3]} onClick={() => clicked(3)} />
+        <Game className="b-bottom-right" state={gameState[4]} onClick={() => clicked(4)} />
+        <Game className="b-bottom" state={gameState[5]} onClick={() => clicked(5)} />
       </div>
       <div className='row jc-center'>
-        <Game className="b-right" state={gameState[6]} />
-        <Game className="b-right" state={gameState[7]} />
-        <Game state={gameState[8]} />
+        <Game className="b-right" state={gameState[6]} onClick={() => clicked(6)} />
+        <Game className="b-right" state={gameState[7]} onClick={() => clicked(7)} />
+        <Game state={gameState[8]} onClick={() => clicked(8)} />
       </div>
     </div>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,7 @@ import React from 'react';
 const game = (props) => {
     const classes = props.className ? `${props.className} square` : 'square';
     return (
-        <span className={classes}>
+        <span className={classes} onClick={props.onClick}>
             {props.state}
         </span>
     );
