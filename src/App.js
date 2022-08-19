697a6a497842cf5576251ac4043144ff2e0d48a8
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-08-19 23:26:30 +0600

    compete condition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import '../src/components/style.css'
 import Game from '../src/components/Game'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const initialState = ['', '', '', '', '', '', '', '', ''];
 
 function App() {
@@ -16,6 +16,34 @@ function App() {
     setChangeState(!changeState)
     setGAmeState(strings)
 
+  }
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      alert(`$(winner) has own the game`)
+      setGAmeState(initialState);
+    }
+  }, [gameState]);
+
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+        return gameState[a];
+      }
+    }
+    return null;
   }
   return (
     <div className="app-header">
